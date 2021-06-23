4bb7d4542161a0563291b9511a835cc446fde729
Marius Jurja <marius.jurja@hype.ro>
2021-06-23 18:37:20 +0300

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dd5674a9165c819a65af2e01a749acb3'),
+    },
+  },
+});
